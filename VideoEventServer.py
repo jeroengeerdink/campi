ef0b4f397bb21d8d3a9e6e1635fcb38e0cbb3c68
Geerdink <geerj@L30535OEU.local>
2019-06-26 18:18:23 +0200

    updated

---
+++ VideoEventServer.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+#Author: Robert H Cudmore
+#Web: http://robertcudmore.org
+#Date: 20151205
+#Purpose: A rewrite of VideoThread.py
+#
+#This should not use pins but respond to function calls instead
+
+'''
+Usage
+==========
+import VideoServer
+v=VideoServer.VideoServer()
+
+#start video server daemon
+v.daemon = True
+v.start()
+
+v.startArm()
+
+#start and stop video recording as much as you like
+v.startVideo()
+v.stopVideo()
+
+v.startVideo()
+v.stopVideo()
+
+v.doTimelapse=1
+v.doTimelapse=0
+
+v.stopArm()
+'''
+import os, time, io, math, threading
+from datetime import datetime #to get fractional seconds
+import picamera
+
+class VideoServer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+
+
+        #when this script is running, it will always be armed
+        self.isArmed = 0
+        self.videoStarted = 0
+
+        self.stream = None
+
+        self.savepath = '/home/pi/video/' + time.strftime('%Y%m%d') + '/'
+        if not os.path.exists(self.savepath):
+            print '\tVideoServer is making output directory:', self.savepath
+            os.makedirs(self.savepath)
+
+        self.savename = '' # the prefix to save all files
+        self.bufferSeconds = 5
+
+        self.startTime = 0 #when we start recording in run(), triggered by startVideo()
+        self.recordDuration = 10 #seconds, set to infinity to then stop with stopVideo()
+
+        self.doTimelapse = 0
+
+    def getState(self):
+        ret = 'a=b'
+        ret += '\nc=d'
+        return ret
+
+    def startArm(self):
+        print '\tVideoThread startArm()'
+        if self.isArmed == 0:
+            print '\tVideoServer initializing camera'
+            self.camera = picamera.PiCamera()
+            self.camera.resolution = (640, 480)
+            self.camera.rotation = 180
+            #self.camera.led = 0
+            self.camera.start_preview()
+            self.camera.framerate = 30 # can be 60
+
+            print '\tVideoServer starting circular stream'
+            self.stream = picamera.PiCameraCircularIO(self.camera, seconds=self.bufferSeconds)
+            self.camera.start_recording(self.stream, format='h264')
+
+            self.isArmed = 1 #order is important, must come after we instantiate camera
+
+    def stopArm(self):
+        print '\tVideoThread stopArm()'
+        timestamp = self.GetTimestamp()
+        if self.isArmed == 1:
+            self.isArmed = 0
+
+            self.camera.stop_preview()
+            self.camera.close()
+            self.camera = None
+
+            self.scanImageFrame = 0
+            print '\tVideoServer stopArm() is done'
+
+
+    def GetTimestamp(self):
+        #returns integer seconds (for file names)
+        return time.strftime('%Y%m%d') + '_' + time.strftime('%H%M%S')
+
+    def GetTimestamp2(self):
+        # returns fraction seconds (for log file entries)
+        #datetime.datetime.now().strftime("%H:%M:%S.%f")
+        return time.strftime('%Y%m%d') + '_' + datetime.now().strftime("%H%M%S.%f")
+
+    #called from run()
+    def write_video(self, stream, beforeFilePath):
+        # Write the entire content of the circular buffer to disk. No need to
+        # lock the stream here as we're definitely not writing to it
+        # simultaneously
+        #with io.open(self.savepath + 'before.h264', 'wb') as output:
+        with io.open(beforeFilePath, 'wb') as output:
+            for frame in stream.frames:
+                if frame.frame_type == picamera.PiVideoFrameType.sps_header:
+                    stream.seek(frame.position)
+                    break
+            while True:
+                buf = stream.read1()
+                if not buf:
+                    break
+                output.write(buf)
+        # Wipe the circular stream once we're done
+        stream.seek(0)
+        stream.truncate()
+
+    def saveBuffer(self):
+        self.recordDuration = 20 #seconds, make this a parameter
+        timestamp = self.GetTimestamp()
+        self.savename = timestamp.split('.')[0]
+        self.scanImageFrame = 0
+        fileName = self.savepath + self.savename
+        self.write_video(self.stream, fileName + ".h264")
+        self.camera.split_recording(self.stream)
+        return fileName
+
+    #start arm
+    def run(self):
+        print '\tVideoServer run() is initializing [can only call this once]'
+        lasttime = time.time()
+        while True:
+            if self.isArmed:
+                timestamp = self.GetTimestamp()
+                while (self.isArmed):
+                    try:
+                        self.camera.wait_recording(0.005)
+                    except:
+                        print '\tVideoServer except clause -->>ERROR'
+                print '\tVideoServer.run fell out of loop'
+            time.sleep(0.05)
+        print '\tVideoServer terminating [is never called]'
---
+++ server.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# Author: Robert H Cudmore
+# Web: http://robertcudmore.org
+# Date: 20151205
+#Purpose: Run a Flask webserver to provide a REST interface to VideoServer.py
+#
+
+'''
+API
+===
+http://server/
+
+http://server/startarm
+http://server/stoparm
+
+http://server/startvideo
+http://server/stopvideo
+
+http://server/timelapseon
+http://server/timelapseoff
+
+'''
+import time, datetime, platform
+import pprint #to print class members
+import shlex
+import subprocess
+from flask import Flask, jsonify, send_file, redirect
+import os, shutil
+
+
+import VideoEventServer
+
+print 'starting server at:', time.strftime("%m/%d/%y"), time.strftime("%H:%M:%S")
+
+app = Flask(__name__)
+
+print 'starting video server'
+v=VideoEventServer.VideoServer()
+v.daemon = True
+v.start()
+
+def purgeFolder(folder):
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+                #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
+
+def genericresponse():
+    dateStr = time.strftime("%m/%d/%y")
+    timeStr = time.strftime("%H:%M:%S")
+    ret = dateStr + ' ' + timeStr + ' system running on: ' + platform.system() + '<BR>'
+    ret += '<BR>'
+    ret += pprint.pformat(v.__dict__).replace(',','<BR>')
+    return ret
+
+@app.route('/arm', methods=['GET'])
+def startArm():
+    v.startArm()
+    return '{"status": "armed"}'
+
+@app.route('/disarm', methods=['GET'])
+def stopArm():
+    v.stopArm()
+    return '{"status": "disarmed"}'
+
+@app.route('/video')
+def send_lastvideo():
+    if v.savename:
+        basepath = v.savepath + v.savename
+        path = basepath + "_before.h264"
+        return send_file(basepath+".mp4", mimetype='video/mp4', attachment_filename='event.mp4', as_attachment=True, cache_timeout=-1)
+    else:
+        return '{"status": "no video"}'
+
+@app.route('/event')
+def event():
+    filepath = v.saveBuffer()
+    time.sleep(0.5)
+    if filepath:
+        #command = shlex.split("MP4Box -add {} {}.mp4".format(path, basepath))
+        command = "MP4Box -add {}.h264 {}.mp4".format(filepath, filepath)
+        print command
+        try:
+            output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+            #output = subprocess.call(command, shell=True)
+        except subprocess.CalledProcessError as e:
+            print 'FAIL:\ncmd:{}\noutput:{}'.format(e.cmd, e.output)
+        return '{"status": "converted", "path": "'+filepath+'"}'
+    else:
+        return '{"status": "error"}'
+
+#home page
+@app.route('/', methods=['GET'])
+def get_index():
+    ret = pprint.pformat(v.__dict__)
+    ret = ret.replace(',','<BR>')
+    return ret
+
+#start the app/webserver
+if __name__ == "__main__":
+    try:
+        purgeFolder(v.savepath)
+        app.run(host='0.0.0.0', port=5010, debug=True)
+        time.sleep(3)
+        v.startArm()
+        #socketio.run(app, host='0.0.0.0', use_reloader=True)
+        #socketio.run(app, host='0.0.0.0', port=5001, use_reloader=True)
+    except:
+        print 'EXITING AND AT LAST LINE'
+        raise
